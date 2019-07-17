e241dba34a54ad6f8d59d9a32ea51c2b59d14910
weimin yang <yangwm1983@gmail.com>
2019-07-17 00:09:22 -0700

    sign up sign in

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,120 @@
+var $signUpName = $(".signUpName");
+var $signUpPassword = $(".signUpPassword");
+var $signUpPassword2 = $(".signUpPasswordAgain");
+var $signUpEmail = $(".signUpEmail");
+var $signUpSubmit = $(".signUpSubmit");
+
+var $signInSubmit = $(".signInSubmit");
+var $signInName = $(".signInName");
+var $signInPassword = $(".signInPassword");
+
+
+
+
+var getApi = {
+  saveUser: function (user) {
+    return $.ajax({
+      headers: {
+        "Content-Type": "application/json"
+      },
+      type: "POST",
+      url: "api/users",
+      data: JSON.stringify(user)
+    });
+  },
+  findOneUser: function (user_name) {
+    // console.log(user_name);
+    return $.ajax({
+      url: "/api/users/" + user_name,
+      type: "GET"
+    });
+  },
+
+};
+
+var logedInUserId="";
+var submitToLogin = function (event) {
+  event.preventDefault();
+
+  var signInName = $signInName.val().trim();
+// console.log(signInName);
+  // if (!(user.user_name && user.password && user.email )) {
+  //   alert("You must enter an example text and description!");
+  //   return;
+  // }
+
+  getApi.findOneUser(signInName).then(function (data) {
+    console.log(data);
+    if(data.length<1){
+      alert("user name not exist");
+      return;
+    }
+    
+    if (data[0].password === $signInPassword.val().trim()) {
+      logedInUserId=data[0].id;
+      console.log("you are loged in, user id is: "+logedInUserId);
+      $signInName.val("");
+      $signInPassword.val("");
+    }
+    else{
+      alert("password not correct");
+    }
+  });
+
+
+
+};
+
+var submitToSave = function (event) {
+  event.preventDefault();
+
+  var user = {
+    user_name: $signUpName.val().trim(),
+    password: $signUpPassword.val().trim(),
+    email: $signUpEmail.val().trim()
+  };
+var signUpPassword2=$signUpPassword2.val().trim();
+  if (!(user.user_name && user.password && signUpPassword2)) {
+    alert("You must enter user name, password ");
+    return;
+  };
+  if(user.password!=signUpPassword2){
+    alert("The passwords don't match");
+    return;
+  };
+  if(user.password.length<6||user.password.length>12){
+    alert("The password length must be between 6 and 12");
+    return;
+  };
+  var isExist="";
+  getApi.findOneUser(user.user_name).then(function (data) {
+    isExist=data[0].id;
+    
+    if(isExist!=""){
+      alert("user name exist!");
+      return;
+    }
+    else{
+
+      getApi.saveUser(user).then(function () {
+        console.log("you are signed up, go to log in");
+
+        $signUpName.val("");
+        $signUpPassword.val("");
+        $signUpPassword2.val("");
+        $signUpEmail.val("");
+      });
+    }
+  });
+};
+
+$signUpSubmit.on("click", submitToSave);
+$signInSubmit.on("click", submitToLogin);
+
+
+
+
+
 // Get references to page elements
 var $exampleText = $("#example-text");
 var $exampleDescription = $("#example-description");
@@ -6,7 +123,7 @@ var $exampleList = $("#example-list");
 
 // The API object contains methods for each kind of request we'll make
 var API = {
-  saveExample: function(example) {
+  saveExample: function (example) {
     return $.ajax({
       headers: {
         "Content-Type": "application/json"
@@ -16,13 +133,13 @@ var API = {
       data: JSON.stringify(example)
     });
   },
-  getExamples: function() {
+  getExamples: function () {
     return $.ajax({
       url: "api/examples",
       type: "GET"
     });
   },
-  deleteExample: function(id) {
+  deleteExample: function (id) {
     return $.ajax({
       url: "api/examples/" + id,
       type: "DELETE"
@@ -31,9 +148,9 @@ var API = {
 };
 
 // refreshExamples gets new examples from the db and repopulates the list
-var refreshExamples = function() {
-  API.getExamples().then(function(data) {
-    var $examples = data.map(function(example) {
+var refreshExamples = function () {
+  API.getExamples().then(function (data) {
+    var $examples = data.map(function (example) {
       var $a = $("<a>")
         .text(example.text)
         .attr("href", "/example/" + example.id);
@@ -61,7 +178,7 @@ var refreshExamples = function() {
 
 // handleFormSubmit is called whenever we submit a new example
 // Save the new example to the db and refresh the list
-var handleFormSubmit = function(event) {
+var handleFormSubmit = function (event) {
   event.preventDefault();
 
   var example = {
@@ -74,7 +191,7 @@ var handleFormSubmit = function(event) {
     return;
   }
 
-  API.saveExample(example).then(function() {
+  API.saveExample(example).then(function () {
     refreshExamples();
   });
 
@@ -84,12 +201,12 @@ var handleFormSubmit = function(event) {
 
 // handleDeleteBtnClick is called when an example's delete button is clicked
 // Remove the example from the db and refresh the list
-var handleDeleteBtnClick = function() {
+var handleDeleteBtnClick = function () {
   var idToDelete = $(this)
     .parent()
     .attr("data-id");
 
-  API.deleteExample(idToDelete).then(function() {
+  API.deleteExample(idToDelete).then(function () {
     refreshExamples();
   });
 };
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,23 +1,49 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
+  // Create a user
+  app.post("/api/users", function (req, res) {
+    db.Users.create(req.body).then(function (dbUsers) {
+      res.json(dbUsers);
+    });
+  });
+  // Get one user
+  app.get("/api/users/:username", function (req, res) {
+    var username = req.params.username;
+    // console.log(username);
+    db.Users.findAll({
+      where: {
+        user_name: username
+      }
+    }).then(function (user) {
+      res.json(user);
+      // console.log(user);
+    });
+  });
+
+
   // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+  app.get("/api/examples", function (req, res) {
+    db.Example.findAll({}).then(function (dbExamples) {
       res.json(dbExamples);
+      console.log("123");
     });
   });
 
   // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
+  app.post("/api/examples", function (req, res) {
+    db.Example.create(req.body).then(function (dbExample) {
       res.json(dbExample);
     });
   });
 
   // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+  app.delete("/api/examples/:id", function (req, res) {
+    db.Example.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(function (dbExample) {
       res.json(dbExample);
     });
   });
